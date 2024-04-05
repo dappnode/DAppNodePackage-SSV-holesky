ac98c93fac862655a6c418045bed3901636a616d
Mateu Miralles <52827122+mateumiralles@users.noreply.github.com>
2024-04-05 16:45:53 +0200

    Using private key pass given during installation (#18)
    * Use given private key pass in installation
    
    * Use given private key pass in installation
    
    * Fixed dkg entrypoint
    
    * Update pass logic
    
    * Remove old code
    
    * Fix operator
    
    * Fix dkg
    
    * create directories in operator entrypoint
    
    * Fix restoring backup
    
    ---------
    
    Co-authored-by: dappnodedev <diego@dappnode.io>

--- operator/entrypoint.sh
+++ operator/entrypoint.sh
@@ -1,88 +1,161 @@
 #!/bin/bash
 
-OPERATOR_DB_DIR=${OPERATOR_DATA_DIR}/db
-OPERATOR_LOGS_DIR=${OPERATOR_DATA_DIR}/logs
-OPERATOR_CONFIG_DIR=${OPERATOR_DATA_DIR}/config
+export OPERATOR_DB_DIR=${OPERATOR_DATA_DIR}/db
+export OPERATOR_CONFIG_DIR=${OPERATOR_DATA_DIR}/config
+export OPERATOR_LOGS_DIR=${OPERATOR_DATA_DIR}/logs
 
-PRIVATE_KEY_FILE=${OPERATOR_CONFIG_DIR}/encrypted_private_key.json
-PRIVATE_KEY_PASSWORD_FILE=${OPERATOR_CONFIG_DIR}/private_key_password
+export PRIVATE_KEY_FILE=${OPERATOR_CONFIG_DIR}/encrypted_private_key.json
+export PRIVATE_KEY_PASSWORD_FILE=${OPERATOR_CONFIG_DIR}/private_key_password
+export NODE_CONFIG_FILE=${NODE_CONFIG_DIR}/node-config.yml
+export NODE_LOG_FILE=${OPERATOR_LOGS_DIR}/node.log
+
+# Temporary files
+OLD_PRIVATE_KEY_PASSWORD_FILE=${OPERATOR_CONFIG_DIR}/old_private_key_password
 DEFAULT_PRIVATE_KEY_FILE=/encrypted_private_key.json
-NODE_CONFIG_FILE=${OPERATOR_CONFIG_DIR}/node-config.yml
-NODE_LOG_FILE=${OPERATOR_LOGS_DIR}/node.log
+RAW_NODE_YML_CONFIG_FILE=${NODE_CONFIG_DIR}/raw-node-config.yml
 
-# Assign proper value to _DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY.
-case "$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY" in
-"holesky-geth.dnp.dappnode.eth") EXECUTION_LAYER_WS="ws://holesky-geth.dappnode:8546" ;;
-"holesky-nethermind.dnp.dappnode.eth") EXECUTION_LAYER_WS="ws://holesky-nethermind.dappnode:8546" ;;
-"holesky-besu.dnp.dappnode.eth") EXECUTION_LAYER_WS="ws://holesky-besu.dappnode:8546" ;;
-"holesky-erigon.dnp.dappnode.eth") EXECUTION_LAYER_WS="ws://holesky-erigon.dappnode:8545" ;;
-*)
-  echo "Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY. Please confirm that the value is correct."
-  exit 1
-  ;;
-esac
+create_directories() {
+  mkdir -p ${OPERATOR_CONFIG_DIR} ${OPERATOR_DB_DIR} ${OPERATOR_LOGS_DIR}
+}
 
-# Assign proper value to _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY.
-case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY" in
-"prysm-holesky.dnp.dappnode.eth") BEACON_NODE_API="http://beacon-chain.prysm-holesky.dappnode:3500" ;;
-"teku-holesky.dnp.dappnode.eth") BEACON_NODE_API="http://beacon-chain.teku-holesky.dappnode:3500" ;;
-"lighthouse-holesky.dnp.dappnode.eth") BEACON_NODE_API="http://beacon-chain.lighthouse-holesky.dappnode:3500" ;;
-"nimbus-holesky.dnp.dappnode.eth") BEACON_NODE_API="http://beacon-validator.nimbus-holesky.dappnode:4500" ;;
-"lodestar-holesky.dnp.dappnode.eth") BEACON_NODE_API="http://beacon-chain.lodestar-holesky.dappnode:3500" ;;
-*)
-  echo "_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY env is not set properly."
-  exit 1
-  ;;
-esac
-
-if [ ! -f "${PRIVATE_KEY_FILE}" ]; then
-
-  # If the private keys in the default location, move it to the proper location.
-  if [ ! -f "${DEFAULT_PRIVATE_KEY_FILE}" ]; then
-
-    echo "Generating a random password for the operator keys..."
-    openssl rand -base64 12 >${PRIVATE_KEY_PASSWORD_FILE}
-
-    echo "Generating operator keys..."
-    /go/bin/ssvnode generate-operator-keys --password-file ${PRIVATE_KEY_PASSWORD_FILE}
-  fi
-
-  echo "Moving private key to the proper location..."
-  mv ${DEFAULT_PRIVATE_KEY_FILE} ${PRIVATE_KEY_FILE}
-
-else
-  echo "Operator keys already exist"
-fi
-
-# Read JSON from PRIVATE_KEY_FILE and extract the publicKey
-PUBLIC_KEY=$(jq -r '.publicKey' ${PRIVATE_KEY_FILE})
-
-# Post ENR to dappmanager
-curl --connect-timeout 5 \
-  --max-time 10 \
-  --silent \
-  --retry 5 \
-  --retry-delay 0 \
-  --retry-max-time 40 \
-  -X POST "http://dappmanager.dappnode/data-send?key=NodePublicKey&data=${PUBLIC_KEY}" ||
-  {
-    echo -e "[ERROR] failed to post public key to dappmanager\n"
+assign_execution_endpoint() {
+  case "$_DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY" in
+  "holesky-geth.dnp.dappnode.eth")
+    EXECUTION_LAYER_WS="ws://holesky-geth.dappnode:8546"
+    ;;
+  "holesky-nethermind.dnp.dappnode.eth")
+    EXECUTION_LAYER_WS="ws://holesky-nethermind.dappnode:8546"
+    ;;
+  "holesky-besu.dnp.dappnode.eth")
+    EXECUTION_LAYER_WS="ws://holesky-besu.dappnode:8546"
+    ;;
+  "holesky-erigon.dnp.dappnode.eth")
+    EXECUTION_LAYER_WS="ws://holesky-erigon.dappnode:8545"
+    ;;
+  *)
+    echo "[ERROR] Unknown value for _DAPPNODE_GLOBAL_EXECUTION_CLIENT_HOLESKY. Please confirm that the value is correct."
     exit 1
-  }
+    ;;
+  esac
 
-echo -e "\nPUBLIC_KEY=${PUBLIC_KEY}\n"
+  export EXECUTION_LAYER_WS
+}
 
-# Create the node config file.
-yq e -i ".global.LogLevel = strenv(LOG_LEVEL)" "${NODE_CONFIG_FILE}"
-yq e -i ".global.LogFilePath = \"${NODE_LOG_FILE}\"" "${NODE_CONFIG_FILE}"
-yq e -i ".db.Path = \"${OPERATOR_DB_DIR}\"" "${NODE_CONFIG_FILE}"
-yq e -i ".ssv.Network = strenv(NETWORK)" "${NODE_CONFIG_FILE}"
-yq e -i ".ssv.ValidatorOptions.BuilderProposals = (strenv(BUILDER_PROPOSALS) == \"true\")" "${NODE_CONFIG_FILE}"
-yq e -i ".eth2.BeaconNodeAddr = \"${BEACON_NODE_API}\"" "${NODE_CONFIG_FILE}"
-yq e -i ".eth1.ETH1Addr = \"${EXECUTION_LAYER_WS}\"" "${NODE_CONFIG_FILE}"
-yq e -i ".p2p.TCPPort = strenv(P2P_TCP_PORT)" "${NODE_CONFIG_FILE}"
-yq e -i ".p2p.UDPPort = strenv(P2P_UDP_PORT)" "${NODE_CONFIG_FILE}"
-yq e -i ".KeyStore.PrivateKeyFile = \"${PRIVATE_KEY_FILE}\"" "${NODE_CONFIG_FILE}"
-yq e -i ".KeyStore.PasswordFile = \"${PRIVATE_KEY_PASSWORD_FILE}\"" "${NODE_CONFIG_FILE}"
+assign_beacon_endpoint() {
+  case "$_DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY" in
+  "prysm-holesky.dnp.dappnode.eth")
+    BEACON_NODE_API="http://beacon-chain.prysm-holesky.dappnode:3500"
+    ;;
+  "teku-holesky.dnp.dappnode.eth")
+    BEACON_NODE_API="http://beacon-chain.teku-holesky.dappnode:3500"
+    ;;
+  "lighthouse-holesky.dnp.dappnode.eth")
+    BEACON_NODE_API="http://beacon-chain.lighthouse-holesky.dappnode:3500"
+    ;;
+  "nimbus-holesky.dnp.dappnode.eth")
+    BEACON_NODE_API="http://beacon-validator.nimbus-holesky.dappnode:4500"
+    ;;
+  "lodestar-holesky.dnp.dappnode.eth")
+    BEACON_NODE_API="http://beacon-chain.lodestar-holesky.dappnode:3500"
+    ;;
+  *)
+    echo "[ERROR] Unknown value for _DAPPNODE_GLOBAL_CONSENSUS_CLIENT_HOLESKY. Please confirm that the value is correct."
+    exit 1
+    ;;
+  esac
 
-/go/bin/ssvnode start-node --config ${NODE_CONFIG_FILE} ${EXTRA_OPTS}
+  export BEACON_NODE_API
+}
+
+handle_password() {
+  if [ -f "${PRIVATE_KEY_PASSWORD_FILE}" ]; then
+    STORED_PRIVATE_KEY_PASS=$(<"${PRIVATE_KEY_PASSWORD_FILE}")
+    if [ -n "${PRIVATE_KEY_PASS}" ] && [ "${PRIVATE_KEY_PASS}" != "${STORED_PRIVATE_KEY_PASS}" ]; then
+      echo "[INFO] The private key password has changed. Updating it..."
+      mv "${PRIVATE_KEY_FILE}" "${PRIVATE_KEY_FILE}.old"
+      echo "${PRIVATE_KEY_PASS}" >"${PRIVATE_KEY_PASSWORD_FILE}"
+    fi
+  elif [ -n "${PRIVATE_KEY_PASS}" ]; then
+    echo "[INFO] Storing the private key password..."
+    echo "${PRIVATE_KEY_PASS}" >"${PRIVATE_KEY_PASSWORD_FILE}"
+  else
+    echo "[INFO] Generating a random password for the operator keys..."
+    openssl rand -base64 12 >"${PRIVATE_KEY_PASSWORD_FILE}"
+  fi
+}
+
+handle_private_key() {
+  if [ ! -f "${PRIVATE_KEY_FILE}" ]; then
+    if [ ! -f "${DEFAULT_PRIVATE_KEY_FILE}" ]; then
+      echo "[INFO] Generating operator keys..."
+      /go/bin/ssvnode generate-operator-keys --password-file "${PRIVATE_KEY_PASSWORD_FILE}"
+    fi
+    echo "[INFO] Moving private key to the proper location..."
+    mv "${DEFAULT_PRIVATE_KEY_FILE}" "${PRIVATE_KEY_FILE}"
+  else
+    echo "[INFO] Operator keys already exist"
+  fi
+}
+
+post_pubkey_to_dappmanager() {
+  PUBLIC_KEY=$(jq -r '.pubKey' ${PRIVATE_KEY_FILE})
+
+  curl --connect-timeout 5 \
+    --max-time 10 \
+    --silent \
+    --retry 5 \
+    --retry-delay 0 \
+    --retry-max-time 40 \
+    -X POST "http://dappmanager.dappnode/data-send?key=NodePublicKey&data=${PUBLIC_KEY}" ||
+    {
+      echo -e "[ERROR] failed to post public key to dappmanager\n"
+    }
+
+  echo "[INFO] You must use the following public key to register your node on the SSV network:"
+  echo -e "\nPUBLIC_KEY=${PUBLIC_KEY}\n"
+}
+
+create_operator_config() {
+  local raw_json_config_file=${NODE_CONFIG_DIR}/raw-node-config.json
+  local modified_json_config_file=${NODE_CONFIG_DIR}/modified-node-config.json
+
+  echo "[INFO] Converting YAML config to JSON..."
+  ruby -ryaml -rjson -e 'puts JSON.pretty_generate(YAML.load(ARGF))' <"${RAW_NODE_YML_CONFIG_FILE}" >"${raw_json_config_file}"
+
+  echo "[INFO] Modifying the operator config..."
+  jq '.global.LogLevel = env.LOG_LEVEL |
+      .global.LogFilePath = env.NODE_LOG_FILE |
+      .db.Path = env.OPERATOR_DB_DIR |
+      .ssv.Network = env.NETWORK |
+      .ssv.ValidatorOptions.BuilderProposals = (env.BUILDER_PROPOSALS == "true") |
+      .eth2.BeaconNodeAddr = env.BEACON_NODE_API |
+      .eth1.ETH1Addr = env.EXECUTION_LAYER_WS |
+      .p2p.TCPPort = env.P2P_TCP_PORT |
+      .p2p.UDPPort = env.P2P_UDP_PORT |
+      .KeyStore.PrivateKeyFile = env.PRIVATE_KEY_FILE |
+      .KeyStore.PasswordFile = env.PRIVATE_KEY_PASSWORD_FILE' "${raw_json_config_file}" >"${modified_json_config_file}"
+
+  echo "[INFO] Converting JSON config back to YAML..."
+  ruby -ryaml -rjson -e 'puts YAML.dump(JSON.parse(ARGF.read))' <"${modified_json_config_file}" >"${NODE_CONFIG_FILE}"
+
+  echo "[INFO] Cleaning up temporary files..."
+  rm -f "${raw_json_config_file}" "${modified_json_config_file}"
+}
+
+start_operator() {
+  echo "[INFO] Starting SSV operator..."
+  exec /go/bin/ssvnode start-node --config ${NODE_CONFIG_FILE} ${EXTRA_OPTS}
+}
+
+main() {
+  create_directories
+  assign_execution_endpoint
+  assign_beacon_endpoint
+  handle_password
+  handle_private_key
+  post_pubkey_to_dappmanager
+  create_operator_config
+  start_operator
+}
+
+main
